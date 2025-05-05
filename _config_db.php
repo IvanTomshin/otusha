eed56664c35ba4aec72a2f010a6787767953cb9a
IvanTomshin <comrad.tomshin@gmail.com>
2025-05-05 09:43:44 +0000

    redis cache enable

--- _config_db.php
+++ _config_db.php
@@ -5,4 +5,6 @@ $_db_host_w="postgres";
 $_db_host_r1="postgres_slave1";
 $_db_host_r2="postgres_slave2";
 $_db_name="otus";
+$_redis_host="redis";
+
 ?>
--- post/feed/index.php
+++ post/feed/index.php
@@ -13,6 +13,8 @@ function view(&$param)
     /* Если не задан токен пользователя - отдадим ленту случайного пользователя  */
     if (empty($_token)) {
         $rnd_id = (int)rand(1, 200000);
+//        $rnd_id = (int)rand(1, 10);
+//        $rnd_id = 1;
         $sql = "select id, token from public.users where id in (select user_id from users_rnd where id = $rnd_id )";
         $result = pg_query($GLOBALS['db_postgresql_conn_r2'], $sql);
 
@@ -42,21 +44,28 @@ function view(&$param)
     $_posts_data = array();
     $sql = "select * from public.posts where user_id in ( select friend_id from friends where user_id = $auth_user_id ) order by id desc limit 10 offset $_page";
 
+    $redis = new Redis();
+    $redis->connect($GLOBALS['redis_conn'], 6379);
+    $_cache_Key = $auth_user_id . hash("sha256", $sql);
 
-    $_hash_key = $auth_user_id . hash("sha256", $sql);
-
-
-
-    $result = pg_query($GLOBALS['db_postgresql_conn_w'], $sql);
-    $_posts_data = array();
-    if ($result) {
-        while ( $_post_data = pg_fetch_array($result, null, PGSQL_ASSOC ) ) {
-            $_posts_data[] = $_post_data;
+    if ($redis->exists($_cache_Key)) {
+        // Получение данных из кэша
+        $_posts_data = json_decode($redis->get($_cache_Key));
+        $param["message"] = "In cache!";
+    } else {
+        $result = pg_query($GLOBALS['db_postgresql_conn_w'], $sql);
+        $_posts_data = array();
+        if ($result) {
+            while ($_post_data = pg_fetch_array($result, null, PGSQL_ASSOC))
+                $_posts_data[] = $_post_data;
+            // Запись в кеш
+            $redis->set($_cache_Key, json_encode($_posts_data));
+            $redis->expire($_cache_Key, 600);
+            $param["message"] = $sql;
         }
-        $param["success"] = true;
     }
+    $param["success"] = true;
     $param["data"] = $_posts_data;
-    $param["message"] = $sql;
 }
 
 
