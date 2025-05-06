9eda0324f8426aeae6935300c3c309a14a50271d
IvanTomshin <comrad.tomshin@gmail.com>
2025-05-06 10:02:32 +0000

    Добавлена инвалидация кеша при удалении и добавлении друзей и при публикации постов

--- post/feed/index.php
+++ post/feed/index.php
@@ -13,8 +13,9 @@ function view(&$param)
     /* Если не задан токен пользователя - отдадим ленту случайного пользователя  */
     if (empty($_token)) {
         $rnd_id = (int)rand(1, 200000);
-//        $rnd_id = (int)rand(1, 10);
+        $rnd_id = (int)rand(1, 1000);
 //        $rnd_id = 1;
+
         $sql = "select id, token from public.users where id in (select user_id from users_rnd where id = $rnd_id )";
         $result = pg_query($GLOBALS['db_postgresql_conn_r2'], $sql);
 
@@ -43,16 +44,23 @@ function view(&$param)
 //    $notify = pg_get_notify($GLOBALS['db_postgresql_conn_w']);
 
     $_page = (int)$_GET['page'];
-
+//    $_page = 0;
     $_cache_Key = $auth_user_id . ":" . $_page;
 
     $_posts_data = array();
-    if ($GLOBALS['redis']->exists($_cache_Key)) {
+    if (1 == 2 and $GLOBALS['redis']->exists($_cache_Key)) {
         // Получение данных из кэша
         $_posts_data = json_decode($GLOBALS['redis']->get($_cache_Key));
         $param["message"] = "In cache!" ;
     } else {
-        $sql = "select * from public.posts where user_id in ( select friend_id from friends where user_id = $auth_user_id ) order by id desc limit 10 offset $_page";
+//        $sql = "select * from public.posts where user_id in ( select friend_id from friends where user_id = $auth_user_id ) order by id desc limit 10 offset $_page";
+        $sql = "
+        select ROW_NUMBER() OVER (ORDER BY P.id desc) AS row_number, P.user_id, P.dt, P.id, P.msg, U.first_name, U.middle_name, U.last_activity from public.posts P 
+        join public.users_data U on U.id = P.user_id
+        join (select count(*), user_id from public.posts group by user_id) S on S.user_id = P.user_id
+        where P.user_id in ( select friend_id from friends where user_id = $auth_user_id ) order by P.id desc limit 10 offset ".(10*$_page);
+
+
         $result = pg_query($GLOBALS['db_postgresql_conn_w'], $sql);
         if ($result) {
             while ($_post_data = pg_fetch_array($result, null, PGSQL_ASSOC))
