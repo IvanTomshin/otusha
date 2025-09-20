7bd0646aa0566788315ec6f0fcb46ee9621bfe40
IvanTomshin <76430829+IvanTomshin@users.noreply.github.com>
2025-09-20 21:57:56 +0500

    Create _config.php

---
+++ docs/proxy/_config.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE & ~ E_WARNING);
+ini_set("display_errors", "on");
+
+$GLOBALS['db_postgresql_conn_w'] = pg_connect("host=$_db_host_w dbname=$_db_name user=$_db_username password=$_db_password");
+$GLOBALS['db_postgresql_conn_r1'] = pg_connect("host=$_db_host_w dbname=$_db_name user=$_db_username password=$_db_password");
+$GLOBALS['db_postgresql_conn_r2'] = pg_connect("host=$_db_host_w dbname=$_db_name user=$_db_username password=$_db_password");
+$GLOBALS['db_postgresql_conn_hproxy'] = pg_connect("host=$_db_host_w dbname=$_db_name user=$_db_username password=$_db_password port=5433");
+
+?>
